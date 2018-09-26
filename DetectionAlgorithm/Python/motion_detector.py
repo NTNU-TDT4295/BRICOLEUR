751e61ed64e892362b170f543e8a8a150336e2c0
Eirik Vale Aase <e.v.aase@gmail.com>
2018-09-26 07:26:20 +0000

    Use sequence of images, check if approaching or not

--- DetectionAlgorithm/Python/motion_detector.py
+++ DetectionAlgorithm/Python/motion_detector.py
@@ -9,40 +9,63 @@ import datetime
 import imutils
 import time
 import cv2
-
+import time
+entire_file = time.time()
+parsing = time.time()
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help="path to the video file")
 ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
 args = vars(ap.parse_args())
-
+end_parsing = time.time()
+print("parsing = ", (end_parsing - parsing))
 # if the video argument is None, then we are reading from webcam
-if args.get("video", None) is None:
-    vs = VideoStream(src=0).start()
-    time.sleep(2.0)
+#if args.get("video", None) is None:
+#    vs = VideoStream(src=0).start()
+#    time.sleep(2.0)
 
 # otherwise, we are reading from a video file
-else:
-    vs = cv2.VideoCapture(args["video"])
+#else:
+#    vs = cv2.VideoCapture(args["video"])
+get_images = time.time()
+vs = cv2.VideoCapture("/home/xilinx/Code/BRICOLEUR/DetectionAlgorithm/TestData/IMG_%04d.jpg")
+fin_get_images = time.time()
+
+print("fetching images = ", (fin_get_images - get_images))
 
 # initialize the first frame in the video stream
 firstFrame = None
 
+oldBoundingBox = (0, 0, 0, 0)
+
+def compareBoundingBoxes(b1, b2):
+    """
+    Returns true if b1 is bigger than b2, otherwise false
+    """
+    (x1, y1, w1, h1) = b1
+    (x2, y2, w2, h2) = b2
+
+    c1 = w1 + w1 + h1 + h1
+    c2 = w2 + w2 + h2 + h2
+    
+    print("new = ", c1, "old = ", c2, "new > old = ", c1 > c2)
+	
+    return c1 > c2
+startAll = time.time()
 # loop over the frames of the video
 while True:
+    start = time.time()
     # grab the current frame and initialize the occupied/unoccupied
     # text
-    frame = vs.read()
-    frame = frame if args.get("video", None) is None else frame[1]
+    frame = vs.read()[1]
+    #frame = frame if args.get("video", None) is None else frame[1]
     text = "Unoccupied"
-
     # if the frame could not be grabbed, then we have reached the end
     # of the vide
     if frame is None:
         break
-
     # resize the frame, convert it to grayscale, and blur it
-    frame = imutils.resize(frame, width=500)
+    #frame = imutils.resize(frame, width=500)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
@@ -50,7 +73,6 @@ while True:
     if firstFrame is None:
         firstFrame = gray
         continue
-
     # compute the absolute difference between the current frame and
     # first frame
     frameDelta = cv2.absdiff(firstFrame, gray)
@@ -62,7 +84,7 @@ while True:
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
                             cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-
+    newBoundingBox = (0, 0, 0, 0)
     # loop over the contours
     for c in cnts:
         # if the contour is too small, ignore it
@@ -72,26 +94,45 @@ while True:
         # compute the bounding box for the contour, draw it on the frame,
         # and update the text
         (x, y, w, h) = cv2.boundingRect(c)
+        newBoundingBox = (x, y, w, h)
+        #if compareBoundingBoxes((x, y, w, h), oldBoundingBox):
+        #    print("Appraching objec")	
+        
+        #oldBoundingBox = (x, y, w, h)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         text = "Occupied"
+    
+    if compareBoundingBoxes(newBoundingBox, oldBoundingBox):
+        print("Approaching")
+    else:
+        print("Going away")
+    
+    oldBoundingBox = newBoundingBox
+
+    end = time.time()
+
+    print("time for one image = ", (end - start))
 
     # draw the text and timestamp on the frame
-    cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-    cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-                (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+    #cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
+    #            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    #cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
+    #            (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
     # show the frame and record if the user presses a key
-    cv2.imshow("Security Feed", frame)
-    cv2.imshow("Thresh", thresh)
-    cv2.imshow("Frame Delta", frameDelta)
+    cv2.imwrite("/home/xilinx/Code/BRICOLEUR/DetectionAlgorithm/Output/tracked.png", frame)
+    #cv2.imwrite("Thresh", thresh)
+    #cv2.imwrite("Frame Delta", frameDelta)
     key = cv2.waitKey(1) & 0xFF
 
     # if the `q` key is pressed, break from the lop
     if key == ord("q"):
         break
 
+stopAll = time.time()
+print("everything but video in = ", (stopAll - startAll))
 # cleanup the camera and close any open windows
-vs.stop() if args.get("video", None) is None else vs.release()
+#vs.stop() if args.get("video", None) is None else vs.release()
 cv2.destroyAllWindows()
-
+end_entire_file = time.time()
+print("entire file = ", end_entire_file - entire_file)
