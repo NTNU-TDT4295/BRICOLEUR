1ca2ec8dc59bdd775ee7066327bb548dc021e920
Eirik Vale Aase <e.v.aase@gmail.com>
2018-11-11 12:44:41 +0100

    Move some files, start on UART component

--- accelerators/src/main/scala/accelerators/main.scala
+++ accelerators/src/main/scala/accelerators/main.scala
@@ -1,11 +1,11 @@
 package accelerators
 
 import chisel3._
-import helpers.Inverter
 import helpers.FIFO.DummyDataGeneratorAXI
 import accelerators.GaussianBlur.GaussianBlur
 import accelerators.Dilation.Dilation
 import accelerators.Grayscale.Grayscale
+import helpers.Inverter.Inverter
 
 // This is the main to compile all needed modules to verilog, run the compile.sh script and look for them in verilog_files
 object Verilogify extends App{
