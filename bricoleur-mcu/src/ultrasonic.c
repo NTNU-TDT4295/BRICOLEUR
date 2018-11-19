52fd2cf2104f8946d585ccbb60e92d3d120c8bb5
Ola Toft <olatoft@hotmail.com>
2018-11-19 17:38:51 +0100

    Ultrasonic: Fix a bug for 1-element buffers

--- bricoleur-mcu/src/ultrasonic.c
+++ bricoleur-mcu/src/ultrasonic.c
@@ -306,13 +306,11 @@ float getUltrasonicLocalConclusion(Buffer *buffer, Position positions[], unsigne
 	// It doesn't make sense to make a line from one point (and we would
 	// get a division by zero)
 
-	if (isObject(distances) && isMovingPositions(positions, buffer->length)) {
+	if (isObject(distances) && buffer->length > 1 && isMovingPositions(positions, buffer->length)) {
 		// It doesn't make sense to make a line from one point (and we would
 		// get a division by zero)
-		if (buffer->length > 1) {
-			getLine(&line, positions, buffer->length);
-			setConclusion(chanceOfCollision(&line));
-		}
+		getLine(&line, positions, buffer->length);
+		setConclusion(chanceOfCollision(&line));
 
 	} else {
 		// Flush buffer, only keep the last element
